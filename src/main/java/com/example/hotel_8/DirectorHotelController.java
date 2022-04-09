6fdbd34acf77e2b3b0877a4d109dfaff1dff50c5
nikonOMG <mamixkik@gmail.com>
2022-04-09 19:05:06 +0600

    profit and loss

--- src/main/java/com/example/hotel_8/DirectorHotelController.java
+++ src/main/java/com/example/hotel_8/DirectorHotelController.java
@@ -1,15 +1,18 @@
 package com.example.hotel_8;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URL;
-import java.util.ArrayList;
+import java.time.LocalDate;
 import java.util.Date;
 import java.util.ResourceBundle;
 
+import com.itextpdf.text.*;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
 import javafx.application.Platform;
-import javafx.collections.ObservableList;
-import javafx.concurrent.Service;
-import javafx.concurrent.Task;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -21,10 +24,10 @@ import javafx.scene.chart.LineChart;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.ProgressBar;
-import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+
 
 public class DirectorHotelController {
     private Stage stage;
@@ -76,6 +79,9 @@ public class DirectorHotelController {
     private Text workerscount;
 
     @FXML
+    private Button generate;
+
+    @FXML
     private Button profile;
 
 
@@ -102,7 +108,7 @@ public class DirectorHotelController {
                         String[] monthss = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
                         Date d = new Date();
                         int[] test = {4, 5, 4, 3, 23, 5, 4, 6, 8, 3, 2, 4};
-                        int now = d.getMonth();
+                        int now = d.getMonth() + 1;
                         double point = 1.0 / 13;
                         double percent = 0;
                         for (int i = now + 1; i != now; i++) {
@@ -127,9 +133,16 @@ public class DirectorHotelController {
 
 
 
-        financescount.setText(financescount.getText() + Data_work.finances);
+        Data_work.setLoss(String.valueOf(LocalDate.now().getMonth()));
+        Data_work.setProfit(String.valueOf(LocalDate.now().getMonth()));
+
+        financescount.setText(financescount.getText() + Data_work.getFinances());
         workerscount.setText(workerscount.getText() + Data_work.workers_count);
 
+        System.out.println("date");
+
+        System.out.println(LocalDate.now().withDayOfMonth(1));
+        System.out.println(LocalDate.now().withDayOfMonth(LocalDate.now().lengthOfMonth()));
 
 
 
@@ -167,6 +180,67 @@ public class DirectorHotelController {
                 scene.getStylesheets().add("style.css");
                 stage.setScene(scene);
                 stage.show();
+
+
+            }
+        });
+
+
+        generate.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+
+            @Override
+            public void handle(MouseEvent event) {
+//                                    SignInBut.getScene().getWindow().hide();
+                Document document = new Document();
+                try
+                {
+                    PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream(".\\Documents\\AddTableExample.pdf"));
+                    document.open();
+
+                    PdfPTable table = new PdfPTable(3); // 3 columns.
+                    table.setWidthPercentage(100); //Width 100%
+                    table.setSpacingBefore(10f); //Space before table
+                    table.setSpacingAfter(10f); //Space after table
+
+                    //Set Column widths
+                    float[] columnWidths = {1f, 1f, 1f};
+                    table.setWidths(columnWidths);
+
+                    PdfPCell cell1 = new PdfPCell(new Paragraph("Cell 1"));
+                    cell1.setBorderColor(BaseColor.BLUE);
+                    cell1.setPaddingLeft(10);
+                    cell1.setHorizontalAlignment(Element.ALIGN_CENTER);
+                    cell1.setVerticalAlignment(Element.ALIGN_MIDDLE);
+
+                    PdfPCell cell2 = new PdfPCell(new Paragraph("Cell 2"));
+                    cell2.setBorderColor(BaseColor.GREEN);
+                    cell2.setPaddingLeft(10);
+                    cell2.setHorizontalAlignment(Element.ALIGN_CENTER);
+                    cell2.setVerticalAlignment(Element.ALIGN_MIDDLE);
+
+                    PdfPCell cell3 = new PdfPCell(new Paragraph("Cell 3"));
+                    cell3.setBorderColor(BaseColor.RED);
+                    cell3.setPaddingLeft(10);
+                    cell3.setHorizontalAlignment(Element.ALIGN_CENTER);
+                    cell3.setVerticalAlignment(Element.ALIGN_MIDDLE);
+
+                    //To avoid having the cell border and the content overlap, if you are having thick cell borders
+                    //cell1.setUserBorderPadding(true);
+                    //cell2.setUserBorderPadding(true);
+                    //cell3.setUserBorderPadding(true);
+
+                    table.addCell(cell1);
+                    table.addCell(cell2);
+                    table.addCell(cell3);
+
+                    document.add(table);
+
+                    document.close();
+                    writer.close();
+                } catch (Exception e)
+                {
+                    e.printStackTrace();
+                }
 
 
             }
